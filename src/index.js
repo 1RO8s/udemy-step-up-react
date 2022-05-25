969428beb5036cb788d59219a5d20c9efd8f137a
1RO8s@kizzo168 <saki0220hiro+git@gmail.com>
2022-05-25 13:25:14 +0900

    inlineStyleの学習

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import ReactDom from "react-dom";
-import App from "./App";
- 
+import App from "./App2";
+
 ReactDom.render(
   <StrictMode>
     <App />
-  </StrictMode>
-  , document.getElementById("root")
+  </StrictMode>,
+  document.getElementById("root")
 );
-
